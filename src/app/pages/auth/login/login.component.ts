64a229db564b3af978f4a686a54aae7cce243efd
Shivendu <shivendu.amale@galendata.com>
2024-04-04 20:59:39 -0500

    login component for later

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.submitEM.emit(this.form.value);
+    }
+  }
+  @Input() error: string = '';
+
+  @Output() submitEM = new EventEmitter();
+
+}
